7c32cda3091807f3d8809c6cc94c58f9eb0f7d41
William Bergh <william.bergh1@learnet.se>
2019-03-21 14:07:56 +0100

    linked all stylesheets

--- assets/inc/header.inc.php
+++ assets/inc/header.inc.php
@@ -2,9 +2,14 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>William Bergh | Portfolio</title>
+    <link rel="stylesheet" href="/portfolio/assets/css/reset.css">
+    <link rel="stylesheet" href="/portfolio/assets/css/main.css">
+    <link rel="stylesheet" href="/portfolio/assets/css/header.css">
+    <link rel="stylesheet" href="/portfolio/assets/css/footer.css">
   </head>
   <body>
-    <header>
-
+    <header class="header">
+      <h1>header</h1>
     </header>
