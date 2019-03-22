30935fba212e971af4ecb7dafcedc1200236dcb5
William Bergh <william.bergh1@learnet.se>
2019-03-22 09:41:50 +0100

    added fontawesome

--- assets/inc/header.inc.php
+++ assets/inc/header.inc.php
@@ -10,6 +10,9 @@
     <link rel="stylesheet" href="/portfolio/assets/css/footer.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.0/css/all.css" integrity="sha384-Mmxa0mLqhmOeaE8vgOSbKacftZcsNYDjQzuCOm6D02luYSzBG8vpaOykv9lFQ51Y" crossorigin="anonymous">
+
+
   </head>
   <body>
     <header class="header">
