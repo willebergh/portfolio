b23e0914a8ba803898828ed10e497c3a8ceed116
William Bergh <william.bergh1@learnet.se>
2019-03-22 09:45:56 +0100

    added html and some styling

--- index.php
+++ index.php
@@ -2,7 +2,35 @@
   include_once "./assets/inc/header.inc.php";
 ?>
 
-
+  <main class="main">
+    <div class="top_content">
+      <div class="top_content_left">
+        <h1>William Bergh</h1>
+        <div class="top_content_nav">
+          <a class="top_content_nav_item" href="#">About</a>
+          <a class="top_content_nav_item" href="#">Career</a>
+          <a class="top_content_nav_item" href="#">Projects</a>
+          <a class="top_content_nav_item" href="#">Contact</a>
+        </div>
+      </div>
+      <span class="spacer"></span>
+      <div class="top_content_right">
+        <div class="projectViewer">
+          <h1 class="project__title">My Latest Project: SuperTorrents</h1>
+          <img class="project__img" src="./assets/img/706e41e616f912c5f6af0061a2619598.jpg" alt="">
+          <div class="project__description">
+            <p>I built a fully functional torrent site as a school project.</br>
+              Features: Login System, Search functionality, Upload / Download torrents and much more</br>
+              Built with: php, mysql, html and css</p>
+              <span class="fab fa-html5"></span>
+              <span class="fab fa-css3-alt"></span>
+              <span class="fab fa-php"></span>
+              <span class="fas fa-database"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 
 <?php
   include_once "./assets/inc/footer.inc.php";
