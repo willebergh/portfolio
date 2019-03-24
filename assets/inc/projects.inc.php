960d93f2a05c7d8c11cbb49aae3702d59998f6cd
William Bergh <william.bergh1@learnet.se>
2019-03-24 13:43:53 +0100

    added styling to projects section and changed som html

--- assets/inc/projects.inc.php
+++ assets/inc/projects.inc.php
@@ -1,19 +1,15 @@
 <div class="projects">
   <div class="project_card">
-    <div class="project_name">
-      <h1>SuperTorrents</h1>
-    </div>
-    <div class="project_features">
-      <p>Login system, upload & download, search, sort and subscribe.</p>
-    </div>
-    <div class="project_description">
-      <p>I built a fully functional torrent site as a school project.</p>
-    </div>
-    <div class="project_lang">
-      <p>HTML, CSS, PHP, MySQL</p>
-    </div>
-    <div class="project_links">
-      <a href="https://github.com/willebergh/supertorrents">GitHub</a>
+    <img src="./assets/img/706e41e616f912c5f6af0061a2619598.png" alt="SuperTorrents">
+    <div class="project_grid_container">
+      <div class="project_text_content">
+        <h1 class="project_grid_item">SuperTorrents</h1>
+        <p class="project_grid_item">Login system, upload & download, search, sort and subscribe.</p>
+        <p class="project_grid_item">I built a fully functional torrent site as a school project.</p>
+        <p class="project_grid_item">HTML, CSS, PHP, MySQL</p>
+      </div>
+      <a class="project_links" href="https://github.com/willebergh/supertorrents">GitHub</a>
+      <a class="project_links" href="#">Learn more</a>
     </div>
   </div>
 </div>
