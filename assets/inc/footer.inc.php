a9c1d0868608297092da0ea81944eb11f099bab8
William Bergh <william.bergh1@learnet.se>
2019-03-24 12:03:29 +0100

    added footer content and styling

--- assets/inc/footer.inc.php
+++ assets/inc/footer.inc.php
@@ -1,5 +1,27 @@
     <footer class="footer">
-      <h1>footer</h1>
+      <div class="footer_flex">
+        <div class="footer_flex_item">
+          <h1>William Bergh</h1>
+        </div>
+        <div class="footer_flex_item">
+          <h1>Contact</h1>
+          <ul>
+            <li><a href="#">E-mail</a></li>
+            <li><a href="#">GitHub</a></li>
+            <li><a href="#">facebook</a></li>
+          </ul>
+        </div>
+        <div class="footer_flex_item">
+          <h1>Navigation</h1>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Career</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+      </div>
     </footer>
   </body>
 </html>
