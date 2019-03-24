3af1ec16b0c5049912a344bf6978db35219eac05
William Bergh <william.bergh1@learnet.se>
2019-03-24 20:44:05 +0100

    added another placholder

--- assets/inc/projects.inc.php
+++ assets/inc/projects.inc.php
@@ -1,15 +1,31 @@
 <div class="projects">
-  <div class="project_card">
-    <img src="./assets/img/706e41e616f912c5f6af0061a2619598.png" alt="SuperTorrents">
-    <div class="project_grid_container">
-      <div class="project_text_content">
-        <h1 class="project_grid_item">SuperTorrents</h1>
-        <p class="project_grid_item">Login system, upload & download, search, sort and subscribe.</p>
-        <p class="project_grid_item">I built a fully functional torrent site as a school project.</p>
-        <p class="project_grid_item">HTML, CSS, PHP, MySQL</p>
+  <h1 class="section_header_gray">My Projects</h1>
+  <div class="project_card_container">
+    <div class="project_card">
+      <img src="./assets/img/706e41e616f912c5f6af0061a2619598.png" alt="SuperTorrents">
+      <div class="project_grid_container">
+        <div class="project_text_content">
+          <h1 class="project_grid_item">SuperTorrents</h1>
+          <p class="project_grid_item">Login system, upload & download, search, sort and subscribe.</p>
+          <p class="project_grid_item">I built a fully functional torrent site as a school project.</p>
+          <p class="project_grid_item">HTML, CSS, PHP, MySQL</p>
+        </div>
+        <a class="project_links" href="https://github.com/willebergh/supertorrents">GitHub</a>
+        <a class="project_links" href="#">Learn more</a>
       </div>
-      <a class="project_links" href="https://github.com/willebergh/supertorrents">GitHub</a>
-      <a class="project_links" href="#">Learn more</a>
+    </div>
+    <div class="project_card">
+      <img src="./assets/img/706e41e616f912c5f6af0061a2619598.png" alt="SuperTorrents">
+      <div class="project_grid_container">
+        <div class="project_text_content">
+          <h1 class="project_grid_item">SuperTorrents</h1>
+          <p class="project_grid_item">Login system, upload & download, search, sort and subscribe.</p>
+          <p class="project_grid_item">I built a fully functional torrent site as a school project.</p>
+          <p class="project_grid_item">HTML, CSS, PHP, MySQL</p>
+        </div>
+        <a class="project_links" href="https://github.com/willebergh/supertorrents">GitHub</a>
+        <a class="project_links" href="#">Learn more</a>
+      </div>
     </div>
   </div>
 </div>
