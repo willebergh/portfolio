83e1aa0e5e5ccbc99c9677ab2fc2269d9d4f7991
William Bergh <william.bergh1@learnet.se>
2019-03-24 12:56:44 +0100

    created and added basic html for projects section

---
+++ assets/inc/projects.inc.php
@@ -0,0 +1,19 @@
+<div class="projects">
+  <div class="project_card">
+    <div class="project_name">
+      <h1>SuperTorrents</h1>
+    </div>
+    <div class="project_features">
+      <p>Login system, upload & download, search, sort and subscribe.</p>
+    </div>
+    <div class="project_description">
+      <p>I built a fully functional torrent site as a school project.</p>
+    </div>
+    <div class="project_lang">
+      <p>HTML, CSS, PHP, MySQL</p>
+    </div>
+    <div class="project_links">
+      <a href="https://github.com/willebergh/supertorrents">GitHub</a>
+    </div>
+  </div>
+</div>
