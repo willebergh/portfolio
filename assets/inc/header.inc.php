36b3608a267d6946475c2fb71526e6e434dc7ddc
William Bergh <william.bergh1@learnet.se>
2019-03-25 15:46:11 +0100

    created main.js and linked it to the header

--- assets/inc/header.inc.php
+++ assets/inc/header.inc.php
@@ -9,9 +9,10 @@
     <link rel="stylesheet" href="/portfolio/assets/css/header.css">
     <link rel="stylesheet" href="/portfolio/assets/css/footer.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:500" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Roboto:500" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,700" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.0/css/all.css" integrity="sha384-Mmxa0mLqhmOeaE8vgOSbKacftZcsNYDjQzuCOm6D02luYSzBG8vpaOykv9lFQ51Y" crossorigin="anonymous">
+    <script src="./assets/js/main.js"></script>
   </head>
   <body>
     <header class="header">
