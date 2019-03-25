11e9a6a49f568dc1058a4d701e18fb7ce5de0a58
William Bergh <william.bergh1@learnet.se>
2019-03-25 19:58:20 +0100

    added offset to scroll animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,15 @@
 function scroll_home() {
-  var elmnt = document.getElementById('home_section');
-  elmnt.scrollIntoView();
+  var elmnt = document.getElementById('home_section').offsetTop;
+  var scroll_num = elmnt - 64;
+  window.scrollTo(0, scroll_num);
 }
 function scroll_about() {
-  var elmnt = document.getElementById('about_section');
-  elmnt.scrollIntoView();
+  var elmnt = document.getElementById('about_section').offsetTop;
+  var scroll_num = elmnt - 64;
+  window.scrollTo(0, scroll_num);
 }
 function scroll_projects() {
-  var elmnt = document.getElementById('projects_section');
-  elmnt.scrollIntoView();
+  var elmnt = document.getElementById('projects_section').offsetTop;
+  var scroll_num = elmnt - 64;
+  window.scrollTo(0, scroll_num);
 }
