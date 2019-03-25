644adee38ca29c2c254e8c3ce6115091bfa25ec7
William Bergh <william.bergh1@learnet.se>
2019-03-25 11:18:09 +0100

    updated latest project card to new design

--- index.php
+++ index.php
@@ -15,17 +15,17 @@
       </div>
       <span class="spacer"></span>
       <div class="top_content_right">
-        <div class="projectViewer">
-          <h1 class="project__title">My Latest Project: SuperTorrents</h1>
-          <img class="project__img" src="./assets/img/706e41e616f912c5f6af0061a2619598.jpg" alt="">
-          <div class="project__description">
-            <p>I built a fully functional torrent site as a school project.</br>
-              Features: Login System, Search functionality, Upload / Download torrents and much more</br>
-              Built with: php, mysql, html and css</p>
-              <span class="fab fa-html5"></span>
-              <span class="fab fa-css3-alt"></span>
-              <span class="fab fa-php"></span>
-              <span class="fas fa-database"></span>
+        <div class="project_card">
+          <img src="./assets/img/706e41e616f912c5f6af0061a2619598.png" alt="SuperTorrents">
+          <div class="project_grid_container">
+            <div class="project_text_content">
+              <h1 class="project_grid_item">SuperTorrents</h1>
+              <p class="project_grid_item">Login system, upload & download, search, sort and subscribe.</p>
+              <p class="project_grid_item">I built a fully functional torrent site as a school project.</p>
+              <p class="project_grid_item">HTML, CSS, PHP, MySQL</p>
+            </div>
+            <a class="project_links" href="https://github.com/willebergh/supertorrents">GitHub</a>
+            <a class="project_links" href="#">Learn more</a>
           </div>
         </div>
       </div>
