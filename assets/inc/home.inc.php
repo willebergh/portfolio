2ee289822f113a194f538e93082c96ecb7918e1c
William Bergh <william.bergh1@learnet.se>
2019-03-25 13:40:39 +0100

    added home.inc.php with html + styling and changed some fonts variables

---
+++ assets/inc/home.inc.php
@@ -0,0 +1,27 @@
+<div class="home section">
+  <div class="home_container">
+    <div class="home_container_item">
+      <h1 class="home_header">
+        Oh, hello</br>
+        my name is William</br>
+        a self taught web developer.</br>
+        </br>checkout my latest project -->
+      </h1>
+    </div>
+    <div class="home_container_item">
+      <div class="project_card">
+        <img src="./assets/img/706e41e616f912c5f6af0061a2619598.png" alt="SuperTorrents">
+        <div class="project_grid_container">
+          <div class="project_text_content">
+            <h1 class="project_grid_item">SuperTorrents</h1>
+            <p class="project_grid_item">Login system, upload & download, search, sort and subscribe.</p>
+            <p class="project_grid_item">I built a fully functional torrent site as a school project.</p>
+            <p class="project_grid_item">HTML, CSS, PHP, MySQL</p>
+          </div>
+          <a class="project_links" href="https://github.com/willebergh/supertorrents">GitHub</a>
+          <a class="project_links" href="#">Learn more</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
