04f9078313f9a275c51af75904a5b6dbca2612bc
William Bergh <william.bergh1@learnet.se>
2019-03-26 14:07:07 +0100

    added basic html structure, placeholder content and syling

--- assets/inc/contact.inc.php
+++ assets/inc/contact.inc.php
@@ -1,3 +1,20 @@
 <div class="contact section" id="contact_section">
   <h1 class="section_header_gray">Contact</h1>
+  <div class="contact_container">
+
+    <div class="contact_card_container">
+      <div class="contact_card">
+        <div class="contact_card_content">
+          <div class="img_container">
+            <img src="./assets/img/gayman.png" alt="gayman">
+          </div>
+          <div class="text_container">
+            <h1>Name</h1>
+            <p>Some random contact info about denniz</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
 </div>
