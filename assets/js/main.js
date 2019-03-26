ec23b2c697f2a12747da5ddadd9c96475661898d
William Bergh <william.bergh1@learnet.se>
2019-03-26 17:52:40 +0100

    added function to change color of nav item based on scroll position

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,37 @@ function scroll_contact() {
   var scroll_num = elmnt - 64;
   window.scrollTo(0, scroll_num);
 }
+function scroll_position() {
+  var x_scroll_pos = $(window).scrollTop();
+  var home = document.getElementById('header_nav_home');
+  var about = document.getElementById('header_nav_about');
+  var projects = document.getElementById('header_nav_projects');
+  var work = document.getElementById('header_nav_work');
+  var contact = document.getElementById('header_nav_contact');
+
+  if (x_scroll_pos > window.innerHeight / 2) {
+    about.style.color = "var(--main-white-color)";
+    home.style.color = "var(--main-light-gray-color)";
+  } else {
+    home.style.color = "var(--main-white-color)";
+    about.style.color = "var(--main-light-gray-color)";
+  }
+  if (x_scroll_pos > window.innerHeight * 2 - window.innerHeight / 2) {
+    projects.style.color = "var(--main-white-color)";
+    about.style.color = "var(--main-light-gray-color)";
+  } else {
+    projects.style.color = "var(--main-light-gray-color)";
+  }
+  if (x_scroll_pos > window.innerHeight * 3 - window.innerHeight / 2) {
+    work.style.color = "var(--main-white-color)";
+    projects.style.color = "var(--main-light-gray-color)";
+  } else {
+    work.style.color = "var(--main-light-gray-color)";
+  }
+  if (x_scroll_pos > window.innerHeight * 4 - window.innerHeight / 2) {
+    contact.style.color = "var(--main-white-color)";
+    work.style.color = "var(--main-light-gray-color)";
+  } else {
+    contact.style.color = "var(--main-light-gray-color)";
+  }
+}
