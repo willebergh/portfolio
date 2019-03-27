ecdc6bf2d50a5887ff293cd038c2888be1f45248
William Bergh <william.bergh1@learnet.se>
2019-03-27 23:25:31 +0100

    a remake on contact card from scratch

--- assets/inc/contact.inc.php
+++ assets/inc/contact.inc.php
@@ -1,20 +1,46 @@
 <div class="contact section" id="contact_section">
-  <h1 class="section_header_gray">Contact</h1>
-  <div class="contact_container">
 
-    <div class="contact_card_container">
+    <div class="content_card_container">
       <div class="contact_card">
-        <div class="contact_card_content">
-          <div class="img_container">
-            <img src="./assets/img/gayman.png" alt="gayman">
+        <h1 class="section_header_white">Contact Me</h1>
+        <div class="contact_card_flex">
+          <div class="contact_card_left">
+            <div class="contact_card_icons_container">
+              <a href="https://github.com/willebergh" target="_blank"><i class="contact_card_icons fab fa-github"></i></a>
+              <a href="https://www.facebook.com/willeberget" target="_blank"><i class="contact_card_icons fab fa-facebook"></i></a>
+            </div>
           </div>
-          <div class="text_container">
-            <h1>Name</h1>
-            <p>Some random contact info about denniz</p>
+          <div class="contact_card_right">
+            <div class="contact_card_form_container">
+              <form class="contact_card_form">
+                <div class="contact_card_form_input_container">
+                  <input class="contact_card_form_input" type="text" name="name" placeholder="Name">
+                  <span class="contact_card_form_span"></span>
+                </div>
+                <div class="contact_card_form_input_container">
+                  <input class="contact_card_form_input" type="text" name="email" placeholder="E-mail">
+                  <span class="contact_card_form_span"></span>
+                </div>
+                <div class="contact_card_form_input_container">
+                  <input class="contact_card_form_input" type="text" name="conmpany" placeholder="Conpany (optional)">
+                  <span class="contact_card_form_span"></span>
+                </div>
+                <div class="contact_card_form_input_container">
+                  <input class="contact_card_form_input" type="text" name="subject" placeholder="Subject">
+                  <span class="contact_card_form_span"></span>
+                </div>
+                <div class="contact_card_form_input_container">
+                  <input class="contact_card_form_input" type="text" name="message" placeholder="Message">
+                  <span class="contact_card_form_span"></span>
+                </div>
+                <div class="contact_card_form_button_container">
+                  <input class="contact_card_form_button" type="submit" name="button" value="Send">
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
     </div>
 
-  </div>
 </div>
