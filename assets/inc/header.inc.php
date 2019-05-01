0f5e6cff30bc9fba6f6a0ca2e665e6166265c82e
William Bergh <willebergh@outlook.com>
2019-05-01 23:48:05 +0200

    fixed relative paths

--- assets/inc/header.inc.php
+++ assets/inc/header.inc.php
@@ -4,15 +4,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>William Bergh | Portfolio</title>
-    <link rel="stylesheet" href="/portfolio/assets/css/reset.css">
-    <link rel="stylesheet" href="/portfolio/assets/css/main.css">
-    <link rel="stylesheet" href="/portfolio/assets/css/header.css">
-    <link rel="stylesheet" href="/portfolio/assets/css/footer.css">
+    <link rel="stylesheet" href="/assets/css/reset.css">
+    <link rel="stylesheet" href="/assets/css/main.css">
+    <link rel="stylesheet" href="/assets/css/header.css">
+    <link rel="stylesheet" href="/assets/css/footer.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto:500" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,700" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.0/css/all.css" integrity="sha384-Mmxa0mLqhmOeaE8vgOSbKacftZcsNYDjQzuCOm6D02luYSzBG8vpaOykv9lFQ51Y" crossorigin="anonymous">
-    <script src="./assets/js/main.js"></script>
+    <script src="/assets/js/main.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   </head>
   <body onscroll="scroll_position()">
